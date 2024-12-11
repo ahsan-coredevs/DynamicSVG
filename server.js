d1c5f25eb399dad8d24892688f2afbbafddfdb03
ahsan-coredevs <ahsan_kabir@coredevs.ltd>
2024-12-11 19:24:10 +0600

    file saved locally

--- server.js
+++ server.js
@@ -4,22 +4,26 @@ const http = require('http');
 const testSvg = require('./testSvg'); 
 
 const server = http.createServer((req, res) => {
-
-  const dynamicValues = {
-    heading: "BUZZ",
-    invest: "4563",
-    sold: "4342",
-    profit: "2896",
-    percentage: "222.22",
-  };
-
-  const updatedSvg = testSvg(dynamicValues);
-
-  res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
-  res.end(updatedSvg);
-});
-
-
-server.listen(3001, () => {
-  console.log('Server running at http://localhost:3001');
-});
+    if (req.url === '/generate') {
+      const dynamicValues = {
+        heading: "BitCoin",
+        invest: "4563",
+        sold: "4342",
+        profit: "2896",
+        percentage: "222.22",
+      };
+  
+   
+      const updatedSvg = testSvg(dynamicValues);
+  
+      res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
+      res.end(updatedSvg);
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Invalid endpoint');
+    }
+  });
+  
+  server.listen(3001, () => {
+    console.log('Server running at http://localhost:3001');
+  });
--- testSvg.js
+++ testSvg.js
@@ -1,20 +1,32 @@
-// svgUtils.js
-
 const fs = require('fs');
 const path = require('path');
 
 function testSvg({ heading, invest, sold, profit, percentage }) {
+
   const filePath = path.join(__dirname, './textSvg.svg');
-  
   const svgTemplate = fs.readFileSync(filePath, 'utf8');
 
-  return svgTemplate
+  const updatedSvg = svgTemplate
     .replace('{heading}', heading)
     .replace('{invest}', invest)
     .replace('{sold}', sold)
     .replace('{profit}', profit)
     .replace('{percentage}', percentage);
+
+  const outputDir = path.join(__dirname, './output_svgs');
+  const outputFileName = `${heading.toLowerCase()}_stats.svg`;
+  const outputPath = path.join(outputDir, outputFileName);
+
+
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+  }
+
+  fs.writeFileSync(outputPath, updatedSvg, 'utf8');
+  console.log(`SVG saved to: ${outputPath}`);
+
+  
+  return updatedSvg;
 }
 
-// Export the function
-module.exports = testSvg; 
+module.exports = testSvg;
